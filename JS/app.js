eaa3c7248aee7ac6aa9d4de4702efbfb1d827ce5
Kate Dameron <katerj33@gmail.com>
2018-08-21 20:25:48 -0700

    HTML + three random images are dynamically generated when the page loads

---
+++ JS/app.js
@@ -0,0 +1,43 @@
+function getImage(fileName) {
+    this.fileName = fileName;
+    this.voteTotal = 0;
+}
+
+var images = [];
+images.push(new getImage("bag.jpg"));
+images.push(new getImage("banana.jpg"));
+images.push(new getImage("boots.jpg"));
+images.push(new getImage("chair.jpg"));
+images.push(new getImage("cthulhu.jpg"));
+images.push(new getImage("dragon.jpg"));
+images.push(new getImage("pen.jpg"));
+images.push(new getImage("scissors.jpg"));
+images.push(new getImage("shark.jpg"));
+images.push(new getImage("sweep.jpg"));
+images.push(new getImage("unicorn.jpg"));
+images.push(new getImage("usb.jpg"));
+images.push(new getImage("water_can.jpg"));
+images.push(new getImage("wine_glass.jpg"));
+
+
+placeRandomImages = function() {
+    var addImage = document.getElementById('images')
+    var image = document.createElement('img');
+    var index = makeThreeRandomImages(0, images.length);
+    image.setAttribute('src', "img/"+images[index].fileName);
+    addImage.appendChild(image);
+    image = document.createElement('img');
+    index = makeThreeRandomImages(0, images.length);
+    image.setAttribute('src', "img/"+images[index].fileName)
+    addImage.appendChild(image);
+    image = document.createElement('img');
+    index = makeThreeRandomImages(0, images.length);
+    image.setAttribute('src', "img/"+images[index].fileName)
+    addImage.appendChild(image);
+}
+
+function makeThreeRandomImages(min, max) {
+    return Math.floor((Math.random() * (max - min)) + min);
+}
+
+window.addEventListener('load', placeRandomImages);
