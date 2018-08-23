0232b8d625478a6a63ca57f13d172ae782da0671
Kate Dameron <katerj33@gmail.com>
2018-08-22 22:25:44 -0700

    total clicks are counted

--- JS/app.js
+++ JS/app.js
@@ -54,15 +54,32 @@ function makeThreeRandomImages(array) {
 } 
 
 var totalClicks = 0;
-
-function trackClicks(){
+function trackClicks() {
+    //add click to vote totals of each image
     for (var index = 0; index < images.length; index++) {
         if (event.target.attributes[0].value == 'img/'+ images[index].fileName) {
             images[index].voteTotal++
-            console.log(event.target.attributes[0].value + images[index].voteTotal);
-        } 
+            console.log('img/'+ images[index].fileName + images[index].voteTotal);
+        }
     }
-    placeRandomImages();
+    //add click to total clicks
+    totalClicks++
+    console.log(totalClicks);
+    //after 3 clicks - run this function for results page
+    if (totalClicks == 3) {
+        console.log('clicked 3 times');
+        function results() {
+            var addImage = document.getElementById('images');
+            var image = document.createElement('img');
+            image.setAttribute('src', "img/"+images[0].fileName);
+            image.innerText = 'Top Choice';
+            addImage.appendChild(image);
+        }
+    } else { //otherwise, replace with 3 new images with this function
+        placeRandomImages();
+    }   
 }
+
+
 
 window.addEventListener('load', placeRandomImages);
