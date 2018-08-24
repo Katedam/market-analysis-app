65b705b562a456219729be80a4adb254dbea8e2c
Kate Dameron <katerj33@gmail.com>
2018-08-24 01:34:13 -0700

    first part of css layout added

--- JS/app.js
+++ JS/app.js
@@ -1,32 +1,34 @@
 var totalClicks = 0;
 
-function getImage(fileName) {
+function getImage(fileName, voteTotal, name) {
     this.fileName = fileName;
     this.voteTotal = 0;
+    this.name = name;
 }
 
 var images = [];
-images.push(new getImage("bag.jpg"));
-images.push(new getImage("banana.jpg"));
-images.push(new getImage("boots.jpg"));
-images.push(new getImage("chair.jpg"));
-images.push(new getImage("cthulhu.jpg"));
-images.push(new getImage("dragon.jpg"));
-images.push(new getImage("pen.jpg"));
-images.push(new getImage("scissors.jpg"));
-images.push(new getImage("shark.jpg"));
-images.push(new getImage("sweep.jpg"));
-images.push(new getImage("unicorn.jpg"));
-images.push(new getImage("usb.jpg"));
-images.push(new getImage("water_can.jpg"));
-images.push(new getImage("wine_glass.jpg"));
+images.push(new getImage("bag.jpg", 0, "Bag"));
+images.push(new getImage("banana.jpg", 0, "Banana Slicer"));
+images.push(new getImage("boots.jpg", 0, "Rain Boots"));
+images.push(new getImage("chair.jpg", 0, "Red Chair"));
+images.push(new getImage("cthulhu.jpg", 0, "Cthulhu"));
+images.push(new getImage("dragon.jpg", 0, "Dragon"));
+images.push(new getImage("pen.jpg", 0, "Pen Utensils"));
+images.push(new getImage("scissors.jpg", 0, "Pizza Scissors"));
+images.push(new getImage("shark.jpg", 0, "Shark Sleeping Bag"));
+images.push(new getImage("sweep.jpg", 0, "Sweeper Babe"));
+images.push(new getImage("unicorn.jpg", 0, "Unicorn"));
+images.push(new getImage("usb.jpg", 0, "USB Tenticle"));
+images.push(new getImage("water_can.jpg", 0, "Self-watering can"));
+images.push(new getImage("wine_glass.jpg", 0, "Wine Glass"));
 
 
 function placeRandomImages() {
-    var addImage = document.getElementById('images')
+    var addImage = document.getElementById('images');
     var image = document.createElement('img');
     var header = document.getElementsByTagName('header');
     var instructions = document.createElement('h2');
+    var name = document.createElement('h3');
     //clear the page
     addImage.innerText = "";
     header[0].innerText = "";
@@ -34,17 +36,38 @@ function placeRandomImages() {
     //reload images and instructions
     instructions.innerText = "Pick the product you would be most likely to purchase";
     header[0].appendChild(instructions);
+    imageContainer = document.createElement('div');
+    imageContainer.setAttribute('id', 'votingImgContainer');
     image.setAttribute('src', "img/"+images[0].fileName);
-    addImage.appendChild(image);
-    addImage.addEventListener('click', trackClicks);
+    imageContainer.appendChild(image);
+    name = document.createElement('h3');
+    name.setAttribute('class', 'sharpieMarker');
+    name.innerText = images[0].name;
+    imageContainer.appendChild(name);
+    addImage.appendChild(imageContainer);
+    image.addEventListener('click', trackClicks);
+    imageContainer = document.createElement('div');
+    imageContainer.setAttribute('id', 'votingImgContainer');
     image = document.createElement('img');
-    image.setAttribute('src', "img/"+images[1].fileName)
-    addImage.appendChild(image);
-    addImage.addEventListener('click', trackClicks);
+    image.setAttribute('src', "img/"+images[1].fileName);
+    imageContainer.appendChild(image);
+    name = document.createElement('h3');
+    name.setAttribute('class', 'sharpieMarker');
+    name.innerText = images[1].name;
+    imageContainer.appendChild(name);
+    addImage.appendChild(imageContainer);
+    image.addEventListener('click', trackClicks);
+    imageContainer = document.createElement('div');
+    imageContainer.setAttribute('id', 'votingImgContainer');
     image = document.createElement('img');
-    image.setAttribute('src', "img/"+images[2].fileName)
-    addImage.appendChild(image);
-    addImage.addEventListener('click', trackClicks);
+    image.setAttribute('src', "img/"+images[2].fileName);
+    imageContainer.appendChild(image);
+    name = document.createElement('h3');
+    name.setAttribute('class', 'sharpieMarker');
+    name.innerText = images[2].name;
+    imageContainer.appendChild(name);
+    addImage.appendChild(imageContainer);
+    image.addEventListener('click', trackClicks);
 }
 
 function shuffleArray(array) {
@@ -63,7 +86,7 @@ function shuffleArray(array) {
 function showProgress() {
    var bar = document.getElementById("bar");
    var width = parseInt(bar.style.width);
-   if (totalClicks == 15) {
+   if (totalClicks == 3) {
        bar.style.width = '0%';
        var progress = document.getElementById("progress");
        progress.style.width = '0%';
@@ -77,14 +100,21 @@ function showProgress() {
 function resultsPage() {
     var addImage = document.getElementById('images');
     var header = document.getElementsByTagName('header');
+    var table = document.getElementById('table');
     header[0].innerText = '';
     addImage.innerText = '';
     var showResults = document.getElementById('results');
     for (var index = 0; index < images.length; index++) {
+        var imageContainer = document.createElement('div');
+        imageContainer.setAttribute('id', 'imageContainer');
         var image = document.createElement('img');
         image.setAttribute('src', "img/"+images[index].fileName) + ('style', 'width:100px');
-        showResults.appendChild(image);
-        console.log(images[index].fileName + images[index].voteTotal); 
+        imageContainer.appendChild(image);
+        var name = document.createElement('h3');
+        name.setAttribute('class', 'sharpieMarker');
+        name.innerText = images[index].name + "     " + images[index].voteTotal;
+        imageContainer.appendChild(name);
+        showResults.appendChild(imageContainer);
     }
     
 }
@@ -93,11 +123,13 @@ function trackClicks(event) {
     showProgress();
     for (var index = 0; index < images.length; index++) {
         if (event.target.attributes[0].value == 'img/'+ images[index].fileName) {
+            console.log(event.target.attributes[0].value);
             images[index].voteTotal++
+            console.log(images[index].voteTotal);
         }
     }
     totalClicks++
-    if (totalClicks == 15) {
+    if (totalClicks == 3) {
         showProgress();
         resultsPage();
     } else { 
